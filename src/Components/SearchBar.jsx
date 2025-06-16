84a708396b10ee4e4ffbb7520441973614fb3e8f
reaper <rahulatehub@gmail.com>
2025-06-16 22:30:03 +0530

    fixed the issue with searchbar for sm screensize

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import PropTypes from "prop-types";
 
-export default function SearchBar({ onSearch, className, showPlaceholder = true }) {
+export default function SearchBar({
+  onSearch,
+  className,
+  showPlaceholder = true,
+}) {
   const [query, setQuery] = useState("");
 
   useEffect(() => {
@@ -26,18 +30,19 @@ export default function SearchBar({ onSearch, className, showPlaceholder = true 
   };
 
   return (
-    <div className={`relative flex items-center border border-orange-900 rounded-full px-2 py-2 focus-within:ring-2 focus-within:ring-orange-900 ${className}`}>
-    <FaSearch className="text-orange-900 text-lg absolute left-2" />
-    <input
-      type="text"
-      value={query}
-      onChange={handleInputChange}
-      onKeyPress={handleKeyPress}
-      placeholder={showPlaceholder ? "Search ..." : ""}
-      className="flex-grow focus:outline-none text-orange-900 bg-transparent sm:pl-8 md:pl-10 lg:pl-12"
-    />
-  </div>
-  
+    <div
+      className={`relative flex items-center border border-orange-900 rounded-full px-2 py-2 focus-within:ring-2 focus-within:ring-orange-900 ${className}`}
+    >
+      <FaSearch className="text-orange-900 text-lg absolute left-2" />
+      <input
+        type="text"
+        value={query}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+        placeholder={showPlaceholder ? "Search ..." : ""}
+        className="w-20 sm:w-auto flex-grow focus:outline-none text-orange-900 bg-transparent sm:pl-8 md:pl-10 lg:pl-12"
+      />
+    </div>
   );
 }
 // PropTypes for type checking
